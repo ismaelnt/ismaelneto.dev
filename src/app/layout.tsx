bbcdc63cc3fd0d02df5fa75c8f0bb50810041b53
ismaelnt <ismaelnt97@gmail.com>
2024-07-02 07:19:57 -0300

    feat: add header and footer to the home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Ismael Neto",
@@ -12,16 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <head />
-      <body>
+      <body className="min-h-screen bg-background font-sans antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Ismael Neto</h1>
+    <main className="container flex justify-center items-center calc h-[calc(100vh-8rem)] px-4">
+      <h1 className="text-6xl font-bold">Ismael Neto</h1>
     </main>
   );
 }
